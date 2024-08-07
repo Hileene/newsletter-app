a685a5eb9bec011b9d719936387543cc13442635
Hileene <92981899+Hileene@users.noreply.github.com>
2024-08-07 18:55:45 +0200

    Created Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SignupImg from '../../assets/images/illustration-sign-up-mobile.svg'
+import { Box, Typography } from '@mui/material'
+import './header.css'
+
+function Header() {
+  return (
+    <header>
+      <img src={SignupImg} alt="Signup Illustration" className="signup-img" />
+      <Box sx={{ fontFamily: 'Roboto' }}>
+        <div className="signup-content">
+          <Typography
+            sx={{ fontWeight: '700', fontSize: '2.5rem' }}
+            variant="h1"
+          >
+            Stay updated!
+          </Typography>
+          <Typography sx={{ fontWeight: '400', fontSize: '1rem' }}>
+            Join 60,000+ product managers receiving monthly updates on:
+          </Typography>
+        </div>
+      </Box>
+    </header>
+  )
+}
+
+export default Header
