cd9a1f3d01c205a2a46ce04d71c1236c52857171
Hileene <92981899+Hileene@users.noreply.github.com>
2024-08-07 18:57:36 +0200

    Added gloabl styling theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+import App from './App'
+import RobotoRegular from './assets/fonts/Roboto-Regular.ttf'
+import RobotoBold from './assets/fonts/Roboto-Bold.ttf'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*Global Style*/
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto,',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '@font-face': [
+          {
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            src: `url(${RobotoRegular}) format('truetype')`,
+          },
+          {
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontWeight: 700,
+            src: `url(${RobotoBold}) format('truetype')`,
+          },
+        ],
+        body: {
+          display: 'flex',
+          flexDirection: 'column',
+          margin: 0,
+          padding: 0,
+          boxSizing: 'border-box',
+          minHeight: '100vh',
+        },
+      },
+    },
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
