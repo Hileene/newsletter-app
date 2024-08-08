8e46f225def26edb5b18cf96ba8c7f0419e6d87a
Hileene <92981899+Hileene@users.noreply.github.com>
2024-08-08 15:22:57 +0200

    Added IconList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css'
 import { Container } from '@mui/material'
 import Header from './components/Header/Header'
+import IconList from './components/IconList/IconList'
 
 function App() {
   return (
+    /*Customized Material UI components to override default styles*/
     <Container
       sx={{ paddingLeft: '0', paddingRight: '0' }}
       className="signup-container"
     >
       <Header />
+      <IconList />
     </Container>
   )
 }
