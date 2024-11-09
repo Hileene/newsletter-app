b164106b7fc451b226bd2d40ac1e82806ca19d97
Hileene <92981899+Hileene@users.noreply.github.com>
2024-11-09 15:13:35 +0100

    Added label element and className

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -3,14 +3,20 @@ import './form.css'
 
 function Form() {
   return (
-    <form className="form">
+    <form className="signup-form">
+      <label className="signup-form-label" htmlFor="email" name="email">
+        Email address
+      </label>
       <input
         type="email"
         placeholder="email@company.com"
         aria-label="Email address"
+        className="signup-form-input"
         required
       />
-      <button type="submit">Subscribe to monthly newsletter</button>
+      <button type="submit" className="signup-form-btn">
+        Subscribe to monthly newsletter
+      </button>
     </form>
   )
 }
