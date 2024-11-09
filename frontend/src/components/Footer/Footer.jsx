cf8b6b4534fa58775de9cc343eccfa9a09b1d260
Hileene <92981899+Hileene@users.noreply.github.com>
2024-11-09 12:35:13 +0100

    Added clasName to the props

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-function Footer() {
-  return <div>Footer</div>
+function Footer({ className }) {
+  return (
+    <div className={`signup-footer ${className}`}>
+      {' '}
+      Challenge by{' '}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href="https://www.frontendmentor.io/profile/Hileene">Aline Behalal</a>.
+    </div>
+  )
 }
 
 export default Footer
