d33c39706d0b298c971d5c7235aa2d788f1f2458
Hileene <92981899+Hileene@users.noreply.github.com>
2024-12-20 17:46:13 +0100

    Added section , div elements and modal succes logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,44 @@ import Header from './components/Header/Header'
 import IconList from './components/IconList/IconList'
 import Form from './components/Form/Form'
 import Footer from './components/Footer/Footer'
+import Success from './components/Success/Success' // Import du composant Success
+import { useState } from 'react'
 
 function App() {
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  const handleSuccess = () => {
+    setIsSuccess(true) // Affiche la modale
+  }
+
+  const handleDismiss = () => {
+    setIsSuccess(false) // Ferme la modale
+  }
+
   return (
-    <main className="signup-container">
-      <div className="signup-preview">
-        <SignupImg />
-      </div>
-      <div className="signup-content">
-        <Header />
-        <IconList />
-        <Form />
-        <Footer className="signup-footer" />
-      </div>
+    <main>
+      <section className="signup-section">
+        <div className="signup-container">
+          <div className="signup-preview">
+            <SignupImg />
+          </div>
+          <div className={`signup-content ${isSuccess ? 'blurred' : ''}`}>
+            <Header />
+            <IconList />
+            <Form onSuccess={handleSuccess} />
+            <Footer className="signup-footer" />
+          </div>
+
+          {/* Modale Success */}
+          {isSuccess && (
+            <div className="modal-overlay">
+              <div className="modal-content">
+                <Success onDismiss={handleDismiss} />
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
     </main>
   )
 }
